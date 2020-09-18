a485741dab54d83774be4de1082a4a24ea3343ab
ToruNiina <niina.toru.68u@gmail.com>
2020-09-18 23:40:59 +0900

    ci

--- main.cpp
+++ main.cpp
@@ -1,18 +1,14 @@
-#include "toml11/toml.hpp"
 #include <iostream>
 
 int main()
 {
-    auto data = toml::parse("issue128.toml");
-    auto tests = toml::find<std::vector<toml::table>>(data, "test");
-    for(auto& test : tests)
+    const auto* chars = u8"ひらがな";
+
+    while(*chars != '\0')
     {
-        std::cout << test["name"] << std::endl;
+        const unsigned int c(*chars);
+        std::cout << std::hex << c << std::endl;
     }
-
-    std::cout << "============================================================" << std::endl;
-
-    std::cout << std::setw(15) << data << std::endl;
 
     return 0;
 }
